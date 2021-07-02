5e48f50348521057b45ad402a720534340cfdefd
Yazan Shannak <yazansh7@hotmail.com>
2021-07-03 02:13:07 +0300

    style fixes

--- src/models/encoders.py
+++ src/models/encoders.py
@@ -15,11 +15,19 @@ class BaseEncoder(nn.Module):
         pass
 
 
-
-
 class EmbeddingEncoder(BaseEncoder):
-    def __init__(self, vocab_count: int, latent_dim: int, pad_index: int, embedding_dim: int, dropout_ratio: float = 0.5, num_layers: int = 1):
-        super(EmbeddingEncoder, self).__init__(vocab_count=vocab_count, latent_dim=latent_dim, pad_index=pad_index, dropout_ratio=dropout_ratio)
+    def __init__(
+        self,
+        vocab_count: int,
+        latent_dim: int,
+        pad_index: int,
+        embedding_dim: int,
+        dropout_ratio: float = 0.5,
+        num_layers: int = 1,
+    ):
+        super(EmbeddingEncoder, self).__init__(
+            vocab_count=vocab_count, latent_dim=latent_dim, pad_index=pad_index, dropout_ratio=dropout_ratio
+        )
 
         self.embedding_dim = embedding_dim
         self.num_layers = num_layers
@@ -28,7 +36,7 @@ class EmbeddingEncoder(BaseEncoder):
             num_embeddings=self.vocab_count, embedding_dim=self.embedding_dim, padding_idx=self.pad_index
         )
         self.rnn = nn.LSTM(
-            input_size=self.embedding_dim, hidden_size=self.latent_dim, batch_first=True, bidirectional=False, num_layers=self.num_layers
+            input_size=self.embedding_dim, hidden_size=self.latent_dim, batch_first=True, num_layers=self.num_layers
         )
         self.dropout = nn.Dropout(p=self.dropout_ratio)
 
@@ -36,18 +44,22 @@ class EmbeddingEncoder(BaseEncoder):
         output = self.dropout(self.embedding(x))
         output = nn.utils.rnn.pack_padded_sequence(input=output, lengths=x_len, batch_first=True, enforce_sorted=False)
 
-        output, (hidden, cell)  = self.rnn(output)
+        output, (hidden, cell) = self.rnn(output)
         output, _ = nn.utils.rnn.pad_packed_sequence(sequence=output, batch_first=True, padding_value=self.pad_index)
 
         return output, hidden, cell
 
 
 class OneHotEncoder(BaseEncoder):
-    def __init__(self, vocab_count: int, latent_dim: int, pad_index: int, dropout_ratio: float =0.5, num_layers: int = 1):
+    def __init__(
+        self, vocab_count: int, latent_dim: int, pad_index: int, dropout_ratio: float = 0.5, num_layers: int = 1
+    ):
         super(OneHotEncoder, self).__init__(vocab_count, latent_dim, pad_index, dropout_ratio)
 
         self.num_layers = num_layers
-        self.rnn = nn.LSTM(input_size=self.vocab_count, hidden_size=self.latent_dim, batch_first=True, num_layers=self.num_layers)
+        self.rnn = nn.LSTM(
+            input_size=self.vocab_count, hidden_size=self.latent_dim, batch_first=True, num_layers=self.num_layers
+        )
 
     def forward(self, x, x_len):
         x = self._one_hot_pack_input(x, x_len)
@@ -57,9 +69,6 @@ class OneHotEncoder(BaseEncoder):
 
         return output, hidden, cell
 
-
     def _one_hot_pack_input(self, x, x_len):
         output = F.one_hot(x, num_classes=self.vocab_count).float()
         return nn.utils.rnn.pack_padded_sequence(input=output, lengths=x_len, batch_first=True, enforce_sorted=False)
-
-
