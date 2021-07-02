e6b57fc53e7c4ee1c770c0f14bd6d8398b2cbefd
Yazan Shannak <yazansh7@hotmail.com>
2021-07-03 01:27:34 +0300

    whitespace fix

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -9,52 +9,44 @@ from typing import Tuple
 from concurrent.futures import ThreadPoolExecutor
 
 class DataModule(pl.LightningDataModule):
-	def __init__(self, data_dir: str, vocab: Vocab, max_length: int = 115):
-		super(DataModule, self).__init__()
-		self.data_dir = data_dir
-		self.max_length = max_length
-		self.vocab = vocab
+    def __init__(self, data_dir: str, vocab: Vocab, batch_size: int = 16):
+        super(DataModule, self).__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.vocab = vocab
 
-	def setup(self):
-		train_data = self.load_csv_file(type="train")
-		self.train_dataset = TensorDataset(train_data[0], train_data[1])
+    def setup(self):
+        train_data = self.load_csv_file(type="train")
+        self.train_dataset = TensorDataset(*train_data)
 
-		validation_data = self.load_csv_file(type="validation")
-		self.validation_dataset = TensorDataset(validation_data[0], validation_data[1])
+        validation_data = self.load_csv_file(type="validation")
+        self.validation_dataset = TensorDataset(*validation_data)
 
-	def load_csv_file(self, type: str)-> torch.Tensor:
-		filepath = os.path.join(self.data_dir, type + ".csv")
-		sources = []
-		targets = []
-		with open(filepath, "rt") as file:
-			with ThreadPoolExecutor(max_workers=6) as executor:
-				data = list(executor.map(self.parse_line_data, file))
+    def load_csv_file(self, type: str)-> torch.Tensor:
+        filepath = os.path.join(self.data_dir, type + ".csv")
+        sources = []
+        targets = []
+        with open(filepath, "rt") as file:
+            with ThreadPoolExecutor() as executor:
+                data = list(executor.map(self.parse_line_data, file))
 
-		sources, targets = zip(*data)
-		sources = pad_sequence(sources, batch_first=True, padding_value=self.vocab.pad_index)
-		targets = pad_sequence(targets, batch_first=True, padding_value=self.vocab.pad_index)
-		data = pad_sequence([sources, targets], batch_first=True, padding_value=self.vocab.pad_index)
-		return data
+        sources, targets = zip(*data)
+        sources_lenghts = list(map(lambda x: len(x), sources))
+        targets_lenghts = list(map(lambda x: len(x), targets))
 
-	def train_dataloader(self) -> DataLoader:
-		return DataLoader(self.train_dataset, batch_size=8, shuffle=True)
+        sources = pad_sequence(sources, batch_first=True, padding_value=self.vocab.pad_index)
+        targets = pad_sequence(targets, batch_first=True, padding_value=self.vocab.pad_index)
+        data = pad_sequence([sources.permute(1, 0), targets.permute(1, 0)], padding_value=self.vocab.pad_index).permute(1, 2, 0)
+        return data[0], torch.tensor(sources_lenghts), data[1], torch.tensor(targets_lenghts)
 
-	def val_dataloader(self) -> DataLoader:
-		return DataLoader(self.validation_dataset, batch_size=8, shuffle=True)
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
 
-	def parse_line_data(self, line: str) -> Tuple[Tensor, Tensor]:
-		source, target = tuple(map(lambda x: "\t" + x.strip() + "\n", line.split(",")))
-		source = torch.tensor([self.vocab.numericalize(char) for char in source])
-		target = torch.tensor([self.vocab.numericalize(char) for char in target])
-		return source, target
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self.validation_dataset, batch_size=self.batch_size, shuffle=True)
 
-
-
-
-
-
-
-
-
-
-
+    def parse_line_data(self, line: str) -> Tuple[Tensor, Tensor]:
+        source, target = tuple(map(lambda x: "\t" + x.strip() + "\n", line.split(",")))
+        source = torch.tensor([self.vocab.numericalize(char) for char in source])
+        target = torch.tensor([self.vocab.numericalize(char) for char in target])
+        return source, target
